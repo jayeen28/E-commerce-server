8ac912c85a752b6757dcbcf86c1b76785b19cbef
Jayeen28 <jayeen6783@gmail.com>
2022-07-17 00:09:46 +0600

    good practices implemented at user route

--- src/routers/user.js
+++ src/routers/user.js
@@ -10,26 +10,24 @@ const { sendMail } = require('../emails/mailsender');
  * @api {post} /users Create a new user
  */
 router.post('/users', async (req, res) => {
-    const allowedInfo = ['name', 'email', 'password'];
-    const isValid = Object.keys(req.body).every(key => allowedInfo.includes(key));
-    if (!isValid) return res.status(400).send('Invalid request');
-    req.body.role = 'user'
-    const user = await new User(req.body);
     try {
+        const allowedInfo = ['name', 'email', 'password'];
+        const isValid = Object.keys(req.body).every(key => allowedInfo.includes(key));
+        if (!isValid) return res.status(400).send('Invalid request');
+        req.body.role = 'user'
+        req.body.active = true;
+        const user = await new User(req.body);
         let status = await sendMail({
             receiver: user.email,
             subject: "Thanks for joining in!",
             type: "text",
             body: `Welcome to the app, ${user.name}. Please wait till approval.`,
         });
-        if (status) {
-            await user.save();
-            res.status(201).send({ user })
-        } else {
-            res.status(400).send({ error: 'Invalid Email, please try again' })
-        }
+        if (!status) return res.status(400).send({ error: 'Invalid Email, please try again' })
+        await user.save();
+        res.status(201).send(user)
     } catch (e) {
-        res.status(400).send({ error: e.message })
+        res.status(400).send('Unable to signup.')
     }
 })
 
@@ -42,9 +40,7 @@ router.post('/users/login', async (req, res) => {
         const user = await User.findByCredentials(req.body.email, req.body.password);
         const token = await user.generateAuthToken();
         res.send({ user, token })
-    } catch (e) {
-        res.status(400).send(e)
-    }
+    } catch (e) { res.status(500).send('Login failed.') }
 })
 
 /**
@@ -52,15 +48,10 @@ router.post('/users/login', async (req, res) => {
  */
 router.post('/users/logout', auth, async (req, res) => {
     try {
-        req.user.tokens = req.user.tokens.filter((token) => {
-            return token.token !== req.token
-        })
+        req.user.tokens = req.user.tokens.filter((token) => token.token !== req.token)
         await req.user.save()
-
-        res.send()
-    } catch (e) {
-        res.status(500).send()
-    }
+        res.status(200).send()
+    } catch (e) { res.status(500).send('Logout failed.') }
 })
 
 /**
@@ -70,54 +61,40 @@ router.post('/users/logoutAll', auth, async (req, res) => {
     try {
         req.user.tokens = []
         await req.user.save()
-        res.send()
-    } catch (e) {
-        res.status(500).send()
-    }
+        res.status(200).send()
+    } catch (e) { res.status(500).send('Logout from all sessions failed.') }
 })
 
 /**
  * @api {get} /users/me Get user info
  */
-router.get('/users/me', auth, async (req, res) => {
-    res.send(req.user)
-})
+router.get('/users/me', auth, async (req, res) => res.status(200).send(req.user))
 
 /**
  * @api {get} /users get all users
  */
 router.get('/users', auth, async (req, res) => {
-    const match = {}
-    const sort = {}
-    if (req.query.active) {
-        match.active = req.query.active
-    }
-    if (req.query.role) {
-        match.role = req.query.role
-    }
-    if (req.query.sortBy) {
-        const parts = req.query.sortBy.split(':')
-        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
-    }
-    if (req.user.role == `admin`) {
-        try {
-            let users = await User.find(
-                match,
-                null,
-                {
-                    limit: parseInt(req.query.limit),
-                    skip: parseInt(req.query.skip),
-                    sort
-                }
-            ).exec()
-            res.send(users)
-        } catch (e) {
-            res.status(500).send()
+    try {
+        const match = {}
+        const sort = {}
+        if (req.query.active) match.active = req.query.active
+        if (req.query.role) match.role = req.query.role
+        if (req.query.sortBy) {
+            const parts = req.query.sortBy.split(':')
+            sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
         }
-    } else {
-        res.status(401).send()
-    }
-
+        if (req.user.role !== 'admin') return res.status(401).send()
+        let users = await User.find(
+            match,
+            null,
+            {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip),
+                sort
+            }
+        ).exec()
+        res.status(200).send(users)
+    } catch (e) { res.status(500).send('Failed to get all users.') }
 })
 
 /**
@@ -133,30 +110,23 @@ router.get('/users/:id', auth, async (req, res) => {
         delete user.tokens
         delete user.password
         delete user.updatedAt
-        res.send(user)
-    } catch (error) {
-        res.status(500).send()
-    }
-
+        res.status(200).send(user)
+    } catch (error) { res.status(500).send('Failed to get user profile.') }
 })
+
 /**
  * @api {patch} /users/:id update current logged in user
  */
 router.patch('/users/me', auth, async (req, res) => {
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['name', 'email', 'password', 'dob', 'avatar', 'designation']
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
-
-    if (!isValidOperation) {
-        return res.status(400).send({ error: 'Invalid updates!' })
-    }
     try {
+        const updates = Object.keys(req.body)
+        const allowedUpdates = ['name', 'email', 'password', 'dob', 'avatar', 'designation']
+        const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+        if (!isValidOperation) return res.status(400).send({ error: 'Invalid updates!' })
         updates.forEach((update) => req.user[update] = req.body[update])
         await req.user.save()
-        res.send(req.user)
-    } catch (e) {
-        res.status(400).send(e)
-    }
+        res.status(200).send(req.user)
+    } catch (e) { res.status(400).send('Failed to update your profile.') }
 })
 
 /**
@@ -164,26 +134,17 @@ router.patch('/users/me', auth, async (req, res) => {
  * @apiParam {String} id user id
  */
 router.patch('/users/:id', auth, async (req, res) => {
-    if (req.user.role == "admin") {
+    try {
+        if (req.user.role !== "admin") return res.status(400).send(`You don't have permission to access this page`)
         const targetUser = await User.findOne({ _id: req.params.id })
         const updates = Object.keys(req.body)
         const allowedUpdates = ['name', 'email', 'password', 'dob', 'role', 'active']
         const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
-        if (!isValidOperation) {
-            return res.status(400).send({ error: 'Invalid updates!' })
-        }
-
-        try {
-            updates.forEach((update) => targetUser[update] = req.body[update])
-            await targetUser.save()
-            res.send(targetUser)
-        } catch (e) {
-            res.status(400).send(e)
-        }
-    } else {
-        res.status(400).send(`You don't have permission to access this page`)
-    }
-
+        if (!isValidOperation) return res.status(400).send({ error: 'Invalid updates!' })
+        updates.forEach((update) => targetUser[update] = req.body[update])
+        await targetUser.save()
+        res.status(200).send(targetUser)
+    } catch (e) { res.status(500).send('Failed to update the user.') }
 })
 
 /**
@@ -193,34 +154,24 @@ router.patch('/users/:id', auth, async (req, res) => {
  * NOTE: Dont need body while deleting user. 
  */
 router.post('/user/:id/:action', auth, async (req, res) => {
-    const action = {
-        active: (user, status) => {
-            user.active = status
-            user.save()
-            res.send(user)
-        },
-        delete: (user) => {
-            user.remove()
-            res.send(user)
-        }
-    }
     try {
-        if (req.user.role = 'admin') {
-            const user = await User.findById(req.params.id)
-            if (user && typeof req.body.status === 'boolean') {
-                action[req.params.action](user, req.body.status)
-            }
-            else {
-                res.status(400).send({ error: 'Something went wrong please try again.' })
+        const action = {
+            active: (user, status) => {
+                user.active = status
+                user.save()
+                res.status(200).send(user)
+            },
+            delete: (user) => {
+                user.remove()
+                res.status(200).send(user)
             }
         }
-        else {
-            res.status(400).send(`Unauthorized`)
-        }
+        if (req.user.role !== 'admin') return res.status(400).send(`Unauthorized`)
+        const user = await User.findById(req.params.id)
+        if (!user && typeof req.body.status !== 'boolean') return res.status(400).send({ error: 'Something went wrong please try again.' })
+        action[req.params.action](user, req.body.status)
     }
-    catch (e) {
-        res.status(500).send()
-    }
+    catch (e) { res.status(500).send('Failed to do the action.') }
 });
 
 const upload = multer({
@@ -228,10 +179,7 @@ const upload = multer({
         fileSize: 1000000
     },
     fileFilter(req, file, cb) {
-        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
-            return cb(new Error('Please upload an image'))
-        }
-
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) return cb(new Error('Please upload an image'))
         cb(undefined, true)
     }
 })
@@ -240,7 +188,7 @@ router.post('/user/me/avatar', auth, upload.single('avatar'), async (req, res) =
     const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
     req.user.avatar = buffer
     await req.user.save()
-    res.send()
+    res.status(200).send()
 }, (error, req, res, next) => {
     res.status(400).send({ error: error.message })
 })
@@ -248,35 +196,25 @@ router.post('/user/me/avatar', auth, upload.single('avatar'), async (req, res) =
 router.delete('/user/me/avatar', auth, async (req, res) => {
     req.user.avatar = undefined
     await req.user.save()
-    res.send()
+    res.status(200).send()
 })
 
 router.get('/user/:id/avatar', async (req, res) => {
     try {
         const user = await User.findById(req.params.id)
-
-        if (!user || !user.avatar) {
-            throw new Error()
-        }
+        if (!user || !user.avatar) throw new Error()
         res.set('Content-Type', 'image/png')
-        res.send(user.avatar)
-    } catch (e) {
-        res.status(404).send()
-    }
+        res.status(200).send(user.avatar)
+    } catch (e) { res.status(404).send('Failed to get the avatar.') }
 })
 
 router.get('/user/:id/profile', async (req, res) => {
     try {
         const user = await User.findById(req.params.id)
-
-        if (!user) {
-            throw new Error()
-        }
+        if (!user) throw new Error()
         // res.set('Content-Type', 'image/png')
-        res.send({ name: user.name, avatar: user.avatar })
-    } catch (e) {
-        res.status(404).send()
-    }
+        res.status(200).send({ name: user.name, avatar: user.avatar })
+    } catch (e) { res.status(404).send('Failed to get user profile.') }
 });
 
 module.exports = router
