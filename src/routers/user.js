7e3db5f5e119fd5823bf40e1e7d91f5338075297
Jayeen28 <jayeen6783@gmail.com>
2022-07-17 14:54:01 +0600

    cleanup

--- src/routers/user.js
+++ src/routers/user.js
@@ -37,7 +37,7 @@ router.post('/users/login', async (req, res) => {
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password);
         const token = await user.generateAuthToken();
-        res.send({ user, token })
+        res.status(200).send({ user, token })
     } catch (e) { res.status(500).send('Login failed.') }
 })
 
